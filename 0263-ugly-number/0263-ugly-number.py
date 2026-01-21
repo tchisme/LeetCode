27e08d1eccd0021f6938989aa65ea754e451ecec
tchisme <149661920+tchisme@users.noreply.github.com>
2026-01-21 10:03:59 +0800

    Time: 0 ms (100%), Space: 12.4 MB (82.33%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isUgly(self, n):
+        if n <= 0:
+            return False
+
+        for p in [2, 3, 5]:
+            while n % p == 0:
+                n = n // p 
+        return n == 1
+        
+        
